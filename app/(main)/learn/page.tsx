b015b4353046d09723f738edf9f076712f98f32a
Tarek <saadfouad1976t@gmail.com>
2025-04-02 20:08:50 +0200

    display lessons and make a service logics that facilate handle frontend UX functionalities

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,39 +1,67 @@
-import { getUserProgress } from '@/db/queries'
+import { getCourseProgress, getUnits, getUserProgress , getLessonPercentage } from "@/db/queries";
 
-import FeedWrapper from '@/components/feed-wrapper'
-import StickyWrapper from '@/components/sticky-wrapper'
-import Header from './header' 
-import UserProgress from '@/components/user-progress'
-import { redirect } from 'next/navigation';
-
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+import Header from "./header";
+import UserProgress from "@/components/user-progress";
+import { redirect } from "next/navigation";
+import { Unit } from "./unit";
 
 export default async function LearnPage() {
-  const userProgress = await getUserProgress();
+  const userProgressData = getUserProgress();
 
-  if (!userProgress || !userProgress.activeCourseId || !userProgress.activeCourse) {
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
+
+  const unitsData = getUnits();
+
+  const [
+    userProgress,
+    units , 
+    courseProgress,
+    lessonPercentage,
+  ] = await Promise.all([userProgressData, unitsData , courseProgressData, lessonPercentageData]);
+
+  if (
+    !userProgress ||
+    !userProgress.activeCourseId ||
+    !userProgress.activeCourse
+  ) {
+    redirect("/courses");
+  }
+
+  if (!courseProgress) {
     redirect("/courses");
   }
 
   return (
-    <div className='flex flex-row-reverse gap-[48px] px-6'>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-        activeCourse={userProgress.activeCourse!}
-        hearts={userProgress.hearts}
-        points={userProgress.points}
-        hasActiveSubscription={false}
+          activeCourse={userProgress.activeCourse!}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
-        <div className='flex flex-col gap-y-4'>
-          <div className='flex flex-col gap-y-2'>
-            <h2 className='text-2xl font-bold'>Welcome to Flutter</h2>
-            <div className='h-[700px] bg-blue-500 w-full bg-border/50' />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={courseProgress.activelesson}  
+              activeLessonPercentage={lessonPercentage} 
+            />
+
           </div>
-          
-        </div>
+        ))}
+
       </FeedWrapper>
     </div>
-  )
+  );
 }
