50bae5696ec473a111ce5a9548a66cc7bb30e2ef
Tarek <saadfouad1976t@gmail.com>
2025-04-05 12:01:40 +0200

    image

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -1,19 +1,19 @@
 import { getLesson, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Challenge } from "./challenge";
+import { toast } from "sonner";
 
 const LessonPage = async () => {
   const lessonData = getLesson();
   const userProgressData = getUserProgress();
 
   const [lesson, userProgress] = await Promise.all([
-    // Corrected the array destructuring and fixed the missing variable assignment
     lessonData,
     userProgressData,
   ]);
 
   if (!lesson || !userProgress) {
-    // Fixed the condition to properly check for missing data
+    toast.error("Lesson not found or user progress unavailable.");
     redirect("/learn");
   }
 
@@ -36,3 +36,13 @@ const LessonPage = async () => {
 };
 
 export default LessonPage;
+
+/*
+Data Loading: When the user enters the lesson page, the application begins loading the user's lesson and progress data from the database.
+
+Data Validation: After the data is loaded, we check whether it is present. If the data is missing, the user is redirected to another page that displays other lessons or asks them to register in the application.
+
+Percentage Calculation: Once we have the data, we begin filtering the completed challenges and calculate the percentage of challenges the user has completed out of the total challenges in the lesson.
+
+Data Passing to the Component: Finally, we pass this data to the Challenge component, which displays the remaining challenges to the user, along with the completion percentage and other information that helps personalize the learning experience.
+*/
