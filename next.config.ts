35954d289692e69ae3645eddeb7a6f20c645f965
Tarek <saadfouad1976t@gmail.com>
2025-04-08 09:04:05 +0200

    nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
   reactStrictMode: true,
   experimental: {
     serverActions: {
